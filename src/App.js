11d34b218b3379a27c896f8b53fec5e7a622965c
banchego <nbnikitanb@gmail.com>
2022-09-15 17:30:41 -0600

    minor updates

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import Navbar  from './components/navbar/NavBar';
+import React from 'react';
+import Navbar from './components/navbar/Navbar';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// import { Home } from './components/pages/Home';
+// import {Universities} from './components/pages/Universities';
+
+
+
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-    </div>
+    <>
+      <Router>
+      <Navbar />
+
+      <div className='pages'>
+      <Routes>
+        {/* <Route exact path="/" element={<Home/>} />
+        <Route path="/universities" element={<Universities />}/> */}
+          
+      </Routes>
+      </div>
+      </Router>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,20 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { configureStore } from '@reduxjs/toolkit'
-import { rootReducer } from './store/reducers';
 
-const store= configureStore(rootReducer);
+// import { Provider } from 'react-redux';
+// import store from './store/store';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-    <App />
-    </Provider>
-  </React.StrictMode>
+    
+        <App />
+    
+    
 );
-
 
