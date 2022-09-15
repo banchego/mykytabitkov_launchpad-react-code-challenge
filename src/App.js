c44a75d59a53c5b47b921454f0680d71cfe79152
banchego <nbnikitanb@gmail.com>
2022-09-14 18:57:55 -0600

    Finished Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-
+import React from 'react';
+import NavBar from './components/navbar/NavBar';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// import { Home } from './components/pages/Home';
+// import {Universities} from './components/pages/Universities';
+
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Router>
+      <NavBar />
+
+      <div className='pages'>
+      <Routes>
+        {/* <Route exact path="/" element={<Home/>} />
+        <Route path="/universities" element={<Universities />}/> */}
+          
+      </Routes>
+      </div>
+      </Router>
+    </>
   );
 }
 
---
+++ src/store/store.js
@@ -0,0 +1,14 @@
+// import { configureStore } from '@reduxjs/toolkit';
+// import postsReducer from '';
+// import universitiesReducer from '';
+// import countriesReducer from '';
+// import postalReducer from '';
+
+// export const store = configureStore({
+//   reducer: {
+//     posts: postsReducer,
+//     universities: universitiesReducer,
+//     countries: countriesReducer,
+//     postal: postalReducer,
+//   },
+// });
