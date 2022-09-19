5cae622c73434a62970012a24cc1d8e999a49ab3
banchego <nbnikitanb@gmail.com>
2022-09-19 16:54:01 -0600

    PostalLookup.js added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/navbar/Navbar'
+import PostalLookup from './pages/postallookup/PostalLookup';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
+      <Routes>
+        
+        <Route exact={true} path="/plu" element={<PostalLookup/>} /> 
+      </Routes>
     </div>
   );
 }
