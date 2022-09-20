fe56aa0ecb0e87b733e7de34f4b79bf4fc9da1ef
banchego <nbnikitanb@gmail.com>
2022-09-19 18:45:32 -0600

    finished postalPage

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Navbar from './components/navbar/Navbar'
-import PostalLookup from './pages/postallookup/PostalLookup';
-import React from 'react';
 
+import Navbar from './components/navbar/Navbar'
+import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/home';
 import Universities from './pages/universities/Universities';
+import PostalLookup from './pages/postallookup/PostalLookup';
 
 
 
 function App() {
   return (
-    
-    <>
+        <>
       <Router>
       <Navbar />
-
       <div className='pages'>
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/universities" element={<Universities />}/>
-        <Route exact={true} path="/plu" element={<PostalLookup/>} />
+        <Route path="/postal" element={<PostalLookup/>} />
           
       </Routes>
       </div>
--- src/index.js
+++ src/index.js
@@ -4,8 +4,7 @@ import './index.css';
 import App from './App';
 import store from './pages/home/home';
 
-// import { Provider } from 'react-redux';
-// import store from './store/store';
+
 
 
 
@@ -13,6 +12,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     
         <App store={store}/>
-    
+       
     
 );
